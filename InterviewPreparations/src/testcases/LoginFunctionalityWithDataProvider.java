e38a015caad439e02658c4fa3cbe0e7ed84843b9
Hemlata Sadhanani <hemlata.sadhanani@gmail.com>
2024-12-06 19:26:23 +0530

    Added driver instance

--- InterviewPreparations/src/testcases/LoginFunctionalityWithDataProvider.java
+++ InterviewPreparations/src/testcases/LoginFunctionalityWithDataProvider.java
@@ -1,24 +1,31 @@
 package testcases;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 
 public class LoginFunctionalityWithDataProvider {
 	
+	WebDriver driver;
+	
 	@BeforeClass
 	public void setup()
 	{
+		driver = new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", "./Driver.chromedriver.exe");
+		driver.manage().window().maximize();
 		
 	}
 	@Test
 	public void validatingLoginWithDifferentUserData()
 	{
-		
+		driver.get("https://www.swiggy.com/");
 	}
 	
 	@AfterClass
 	public void tearDown()
 	{
-		
+		driver.quit();
 	}
 
 }
